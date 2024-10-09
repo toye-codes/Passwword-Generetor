e730d62773e50e1d1f13b027502cf556c50175e1
Agbesanya Olusanya <agbesanyaolusanya@gmail.com>
2024-10-09 21:04:48 +0100

    Added script file to make the code function.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const genPassword = document.getElementById("genPassword");
+const password = document.getElementById("password")
+
+const randomPassword = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    genPassword.onclick = function () {
+        let newPassword = "";
+        for ( let i = 0; i < 8; i++) {
+
+        const createRandomPassword = Math.floor(Math.random() * randomPassword.length);
+        newPassword += randomPassword[createRandomPassword];
+    }
+
+        password.value = newPassword;
+    }
